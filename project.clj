d2c5c68ad5132cba1f79bd9469145eddca28d31a
Conner Bryan <conner.bryan@josflare.com>
2015-03-02 10:03:56 -0500

    add puniverse quasar for fibers

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [co.paralleluniverse/pulsar "0.6.2"]]
+  :java-agents [[co.paralleluniverse/quasar-core "0.6.2"]]
   :main ^:skip-aot hydra.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
